431cd764cd477277f82229ee1f6f23da84358d87
AnnaHort <annahort3d@gmail.com>
2023-12-10 21:18:14 +0200

    add redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,12 @@ import { contactsReducer } from "./contactSlice";
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Об'єднуємо всі редуктори які ми хочемо зберегти
+// Об'єднуємо всі редюсери які ми хочемо зберегти
 const rootReducer = combineReducers({
   contacts: contactsReducer,
 });
 
+// збереження стейту у локальному сховищі
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
